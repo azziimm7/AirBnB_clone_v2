87b0fe9297cca8a90bcf25b78693dac62bae08af
azziimm7 <azzah31@outlook.com>
2024-05-17 14:10:24 +0000

    task 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-#script that starts a Flask web application
+'''
+script that starts a Flask web application
+'''
 
-from flask import flask
+from flask import Flask
+app = Flask(__name__)
 
-app = flask(__name__)
-
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
+	"""
+	return hello hbnb
+	"""
 	return "Hello HBNB!"
 
 if __name__ == "__main__":
