91e64cb78b5c6c537513e8ec6333b17f7636c635
azziimm7 <azzah31@outlook.com>
2024-05-17 16:05:20 +0000

    space

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """This function return c is fun + some text"""
-    return 'C' + text.replace('_', '')
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
