06af1fb4befdb38a976b47d44d12c54a55a518c8
azziimm7 <azzah31@outlook.com>
2024-05-19 13:15:34 +0000

    port

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module starts a flask application
+"""
+from flask import Flask, render_template
+from models import storage
+from models import *
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def db(exc):
+    """THis function call method storage.close()"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def stateslist():
+    """ This funxtion displays html tag"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
