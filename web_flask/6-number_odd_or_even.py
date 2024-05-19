471fbf3780110a134dd1bbaff1e67069ce3e55bf
azziimm7 <azzah31@outlook.com>
2024-05-19 08:02:03 +0000

    <

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,7 +45,7 @@ def numbertemplate(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/int:<n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def evenorodd(n):
     """This function return a hrml page with a nymber even or odd"""
     return render_template('6-number_odd_or_even.html', n=n)
